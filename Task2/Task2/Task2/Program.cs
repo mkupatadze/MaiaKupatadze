d4fd1dfd1f8666250f9767dbcd158f6efb0e14bc
Maia Kupatadze <mkupatadze@credo.ge>
2024-10-24 10:45:23 +0400

    Relevant data types

---
+++ Task2/Task2/Task2/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.VisualBasic;
+
+Console.WriteLine("Hello, World!");
+
+//byte b1 = 34;
+//short  = 165;
+//int  =
+//long  = 
+//sbyte
+//uint  
+//bool isActive = true
+
+//float f1 = 65.1;
+
+string FirstName = "Maia";
+string LastName = "Kupatadze";
+var FirstInitial = "M";
+var LastInitial = "K";
+string FullName = FirstName + LastName;
+string FullInitials = FirstInitial + LastInitial;
+int age = 34;
+float height = 165;
+long ID = 35001100110;
+long Mobile = +995599038444;
+bool isEmployed = true;
+string Position = "IT Business Analyst";
+string Company = "JSC Credo Bank";
+
+
+Console.WriteLine("Gamarjoba, My name is " + FullName);
+Console.WriteLine("My Initials is " + FullInitials);
+Console.WriteLine("My age is " + age);
+Console.WriteLine("And my height is " + height);
+Console.WriteLine("I'm working in " + Company + " and my position is " + Position);
+
+
+
+
+
+
+
+
