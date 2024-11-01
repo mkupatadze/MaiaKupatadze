202384a816a192c9bffd766bf18dd936aa8216f1
Maia Kupatadze <mkupatadze@credo.ge>
2024-11-01 11:26:18 +0400

    Task6

---
+++ Task6.1/Task6.1/Program.cs
@@ -0,0 +1,93 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("tamashi");
+        int[] arr = new int[5];
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.WriteLine("sheikvane ritskhvi: ");
+            int num = int.Parse(Console.ReadLine());
+            arr[i] = num;
+
+        }
+        Console.WriteLine("sheikvane indeksi: ");
+        int index = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("shedegi : " + CalculateNumberSum(arr, index));
+
+    }
+
+
+    static int CalculateNumberSum(int[] arr, int index)
+    {
+        int n = 0;
+        {
+            n = arr[index];
+        }
+
+        int result = 0;
+
+        while (true)
+        {
+            if (n == 0)
+            {
+                break;
+            }
+            result += n % 10;
+            n /= 10;
+        }
+        return result;
+
+    }
+}
+
+
+
+
+//static void Main(string[] args)
+//{
+//    Console.WriteLine("tamashi");
+//    int[] arr = new int[5];
+
+//    for (int i = 0; i < arr.Length; i++)
+//    {
+//        Console.WriteLine("sheikvane ritskhvi: ");
+//        int num = int.Parse(Console.ReadLine());
+//        arr[i] = num;  // Assign to arr[i] instead of arr[1]
+//    }
+
+//    Console.WriteLine("sheikvane indeksi: ");
+//    int index = int.Parse(Console.ReadLine());
+
+//    // Validate index
+//    if (index < 0 || index >= arr.Length)
+//    {
+//        Console.WriteLine("Invalid index. Please enter a number between 0 and " + (arr.Length - 1));
+//    }
+//    else
+//    {
+//        Console.WriteLine("shedegi: " + CalculateNumberSum(arr, index));
+//    }
+//}
+
+//static int CalculateNumberSum(int[] arr, int index)
+//{
+//    int n = arr[index];
+//    int result = 0;
+
+//    while (n != 0)
+//    {
+//        result += n % 10;
+//        n /= 10;
+//    }
+//    return result;
+//}
+
+
---
+++ Task6/Program.cs
@@ -0,0 +1,76 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter 5 integers:");
+            int[] arr = new int[5];
+
+            // Input numbers
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write("Input number {0}: ", i + 1);
+                while (true)
+                {
+                    try
+                    {
+                        arr[i] = int.Parse(Console.ReadLine());
+                        break; // Exit loop if parsing is successful
+                    }
+                    catch (FormatException)
+                    {
+                        Console.Write("Invalid input. Please enter an integer: ");
+                    }
+                }
+            }
+
+            // Input index
+            Console.Write("Enter the index (0-4) for logic calculation: ");
+            int index;
+            while (true)
+            {
+                try
+                {
+                    index = int.Parse(Console.ReadLine());
+                    if (index < 0 || index >= arr.Length)
+                    {
+                        throw new IndexOutOfRangeException();
+                    }
+                    break; // Exit loop if index is valid
+                }
+                catch (FormatException)
+                {
+                    Console.Write("Invalid input. Please enter an integer: ");
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    Console.Write("Index out of range. Please enter a valid index (0-4): ");
+                }
+            }
+
+            // Calculate and display result
+            Console.WriteLine("Sum of digits: " + CalculateNumberSum(arr, index));
+        }
+
+        static int CalculateNumberSum(int[] array, int idx)
+        {
+            int number = array[idx];
+
+            // Calculate the sum of the digits
+            int result = 0;
+
+            while (number != 0)
+            {
+                result += number % 10;
+                number /= 10;
+            }
+
+            return result;
+        }
+    }
+}
