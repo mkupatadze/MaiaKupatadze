528bfe68089cde41f1fdb67be40ab82ef90f6be4
Maia Kupatadze <mkupatadze@credo.ge>
2024-11-27 09:52:57 +0400

    interface&abstract

---
+++ Task9/Animal.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task9
+{
+    public interface IAnimal
+    {
+        string MakeSound();
+    }
+
+    public abstract class Animal : IAnimal
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Animal(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        public abstract string MakeSound();
+    }
+
+    public class Dog : Animal
+    {
+        public Dog(string name, int age) : base(name, age)
+        {
+        }
+
+        public override string MakeSound()
+        {
+            return "Woof";
+        }
+    }
+
+    public class Cat : Animal
+    {
+        public Cat(string name, int age) : base(name, age)
+        {
+        }
+
+        public override string MakeSound()
+        {
+            return "Meow";
+        }
+    }
+
+    public class Cow : Animal
+    {
+        public Cow(string name, int age) : base(name, age)
+        {
+        }
+
+        public override string MakeSound()
+        {
+            return "Moo";
+        }
+    }
+}
---
+++ Task9/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using Task9;
+
+class Program
+{
+    static void Main (string[] args)
+    {
+        IAnimal[] animals = new IAnimal[]
+        {
+            new Dog("Gray", 2),
+            new Cat("Bobo", 1),
+            new Cow("Turfa", 5),
+        };
+
+        foreach (var animal in animals)
+        {
+            Console.WriteLine($"{animal.GetType().Name} says: {animal.MakeSound()}");
+        }
+    }
+}
