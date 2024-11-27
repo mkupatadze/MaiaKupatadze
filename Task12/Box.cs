83a8c3b8c336d3da9fe28dc1a2460f62d2449874
Maia Kupatadze <mkupatadze@credo.ge>
2024-11-27 21:56:52 +0400

    Generics

---
+++ Task12/Box.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task12
+{
+    public class Box<T>
+    {
+        private T value;
+
+        public void SetValue(T newValue)
+        {
+            value = newValue;
+        }
+
+        public T GetValue()
+        {
+            return value;
+        }
+    }
+
+    public static class Utility
+    {
+        public static int GetListCount<T>(this List<T> list)
+        {
+            return list.Count;
+        }
+
+    }
+
+
+
+
+
+
+
+}
---
+++ Task12/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+using Task12;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        
+        Box<int> intBox = new Box<int>();
+        intBox.SetValue(17);
+        Console.WriteLine($"Box<int> value: {intBox.GetValue()}");
+        
+
+        Box<string> stringBox = new Box<string>();
+        stringBox.SetValue("Gamarjoba");
+        Console.WriteLine($"Box<string> value: {stringBox.GetValue()}");
+
+        
+        List<int> intList = new List<int> {3, 7, 13 };
+        Console.WriteLine($"List<int> count: {Utility.GetListCount(intList)}");
+
+       
+        List<string> stringList = new List<string> { "n", "d", "e", "a", "m"};
+        Console.WriteLine($"List<string> count: {Utility.GetListCount(stringList)}");
+    }
+}
+
